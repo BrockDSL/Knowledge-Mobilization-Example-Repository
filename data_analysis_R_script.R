599ac233f0b6a802d625043c73d953bd99380863
DanielBrett <44175011+DanielBrett@users.noreply.github.com>
2025-06-05 16:35:33 -0400

    Update data_analysis_R_script.R

--- data_analysis_R_script.R
+++ data_analysis_R_script.R
@@ -2,7 +2,7 @@ library(ggplot2)
 
 data <- read.csv("Data.csv")
 
-plot <- ggplot(data, aes(x=Pets,color="blue")) + 
+plot <- ggplot(data, aes(x=Pets,fill=Pets)) + 
   geom_bar() +
   labs(x = "Entries", y = "Count", title = "Count of Entries in the First Column") +
   theme(panel.background = element_rect(fill = 'white'), plot.title = element_text(hjust = 0.5)) + labs(x="Types of Pets", y="Number of People with that Pet", title="Pet Survey Results")
